6433f8972d372c52038965cc77cc6d70c8c3758c
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-08 01:25:52 +0000

    ユーザー認証機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root'homes#top'
   get'about' =>'homes#show'
 
-  resources :users,only[:show,:edit,:index,:update] do
+  resources :users, only:[:show,:edit,:index,:update] do
    member do
       get :followings, :followers #フォローフォロワー一覧取得
     end
-     resources :relationships,onky[:create,:destroy] #フォロー・フォロー解除
+     resources :relationships, only:[:create,:destroy] #フォロー・フォロー解除
   end
 
   resources :post_images do
-   resources:post_comments,only[:create,:destroy,:index]
-   resources:favorites,only[:create,:destroy,:index]
+   resources:post_comments, only:[:create,:destroy,:index]
+   resources:favorites, only:[:create,:destroy,:index]
   end
 
-  resources :genres,only[:index,:create,:edit,:update]
+  resources :genres, only:[:index,:create,:edit,:update]
 
   post'rooms' =>'rooms#create' #DM機能
   get'rooms/:id' =>'rooms#show' #DM機能
