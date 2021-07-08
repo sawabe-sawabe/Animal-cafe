e57f210b964b9849006aacc9ac06965c807a15fa
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-08 01:01:29 +0000

    ルーディング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root'homes#top'
+  get'about' =>'homes#show'
+
+  resources :users,only[:show,:edit,:index,:update] do
+   member do
+      get :followings, :followers #フォローフォロワー一覧取得
+    end
+     resources :relationships,onky[:create,:destroy] #フォロー・フォロー解除
+  end
+
+  resources :post_images do
+   resources:post_comments,only[:create,:destroy,:index]
+   resources:favorites,only[:create,:destroy,:index]
+  end
+
+  resources :genres,only[:index,:create,:edit,:update]
+
+  post'rooms' =>'rooms#create' #DM機能
+  get'rooms/:id' =>'rooms#show' #DM機能
+
+  post'messages' =>'messages#create'  #DM機能
+
+
 end
