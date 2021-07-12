1888e1f95c8fb0fc62dbc80e4a4f09d4b86ce5c3
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-12 06:17:03 +0000

    いいね機能・コメント機能非同期通信化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,14 +4,14 @@ class FavoritesController < ApplicationController
    post_image = PostImage.find(params[:post_image_id])
    favorite = current_user.favorites.new(post_image_id: post_image.id)
    favorite.save
-   redirect_to post_image_path(post_image)
+   redirect_to request.referer
  end
 
  def destroy
    post_image = PostImage.find(params[:post_image_id])
    favorite = current_user.favorites.find_by(post_image_id: post_image.id)
    favorite.destroy
-   redirect_to post_image_path(post_image)
+   redirect_to request.referer
  end
 
  def index
