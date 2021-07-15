ce02505a08fad65d3ad7538c79b0295bfedad7c3
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-15 10:06:50 +0000

    投稿一覧・コントローラー修正・バリテーション・追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
  has_many :post_images
+ validates :name, presence: true ,length: { maximum: 20 }, uniqueness: true
 end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,6 +4,12 @@ class PostImage < ApplicationRecord
    attachment :image
    has_many :favorites, dependent: :destroy
    has_many :post_comments, dependent: :destroy
+
+   validates :image, presence: true
+   validates :genre_id, presence: true
+   validates :title, presence: true
+
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?  #ユーザーidがテーブル内に存在するか確認するため
   end
