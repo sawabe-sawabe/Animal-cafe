134181f0f224078df8e74cc6fc3c253c50d63b29
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-15 04:37:37 +0000

    新規投稿・投稿編集・いいね一覧・コメント一覧画面修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @images =PostImage.where(user_id: current_user.id)
     @post_images = @user.post_images
-    @post_comments = PostComment.where(user_id: current_user.id)
+    @post_comments = PostComment.where(user_id: @user.id)
   end
 
   def index
