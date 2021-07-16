e9c9bdb1fd4362c04298c731b02e309f9877b146
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-16 08:18:49 +0000

    検索機能コード修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,21 +1,23 @@
 class SearchesController < ApplicationController
  
- def search
-   @range = params[:range]
-   search = params[:search]
-   word = params[:word]
-   if  @range == '1'
-     @user = User.search(search,word)
-   else  @post_image = "2"
-     @post_image = PostImage.search(search,word)
-   end
+  def search
+    @range = params[:range] #rangeパラメーターを使用して、ユーザー検索と投稿検索を振り分けるため
+    search = params[:search] #searchパラメーターを使用して、検索方法を分岐するため
+    word = params[:word]
+   
+   #検索種類の振り分け
+    if  @range == '1'
+      @user = User.search(search,word)
+    else  @post_image = "2"
+      @post_image = PostImage.search(search,word)
+    end
   end
  
- def match
-   value = params[:value]
-   @genre =Genre.find(params[:value].to_i)
-   @post_image = PostImage.match(value)
- end
+  def match #ジャンル検索用
+    value = params[:value] #ジャンルid代入にvalueパラメーターを使用
+    @genre =Genre.find(params[:value].to_i) #ジャンル名を検索結果に表示するため
+    @post_image = PostImage.match(value)
+  end
 
 
  end
