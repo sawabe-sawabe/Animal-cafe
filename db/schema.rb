7953c2c4010cd6d37111fe7837c209ef6a5eff4c
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-17 15:37:16 +0000

    詳細画面編集中

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_074525) do
+ActiveRecord::Schema.define(version: 2021_07_17_143758) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2021_07_11_074525) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string "name" 
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -59,12 +59,12 @@ ActiveRecord::Schema.define(version: 2021_07_11_074525) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "name"
-    t.text "profile_image_url"
     t.string "having_pets"
     t.string "favorite_animal"
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
