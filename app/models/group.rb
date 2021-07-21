b53048b6f731b7ce0a841766f78b2e2156fa1845
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-21 05:24:00 +0000

    修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,9 +3,10 @@ class Group < ApplicationRecord
   has_many:users, through: :group_users
   attachment :profile_image
   attachment :image
-   
+
   def joined_by?(user)
     group_users.where(user_id: user.id).exists? #グループにユーザーが加入しているかを確認
+
   end
 
 
