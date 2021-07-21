6bf2aa19944bfe15af8af38f4cea341139d9a732
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-21 15:13:47 +0000

    いいね機能修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,19 +1,17 @@
 class PostCommentsController < ApplicationController
  before_action :authenticate_user!
  def create
-   post_image = PostImage.find(params[:post_image_id])
-   comment = current_user.post_comments.new(post_comment_params)
-   comment.post_image_id = post_image.id
-   if comment.save
-     redirect_to request.referer
-   else
+   @post_image = PostImage.find(params[:post_image_id])
+   @comment = current_user.post_comments.new(post_comment_params)
+   @comment.post_image_id = @post_image.id
+   unless @comment.save
      render :show
    end
  end
 
  def destroy
-   PostComment.find_by(id:params[:id],post_image_id:params[:post_image_id]).destroy
-   redirect_to request.referer
+   @post_image =PostComment.find_by(id:params[:id],post_image_id:params[:post_image_id]).destroy
+   
  end
 
  def index
