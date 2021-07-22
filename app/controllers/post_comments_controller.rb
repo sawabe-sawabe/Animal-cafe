27c127091bfc2f18ab44ed4a6b4fcacbac9e8208
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-22 01:04:13 +0000

    コメント機能修正

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,16 +2,18 @@ class PostCommentsController < ApplicationController
  before_action :authenticate_user!
  def create
    @post_image = PostImage.find(params[:post_image_id])
-   @comment = current_user.post_comments.new(post_comment_params)
-   @comment.post_image_id = @post_image.id
-   unless @comment.save
+   @post_comment = current_user.post_comments.new(post_comment_params)
+   @post_comment.post_image_id = @post_image.id
+   unless @post_comment.save #空白では投稿できないようにする
      render :show
    end
  end
 
  def destroy
-   @post_image =PostComment.find_by(id:params[:id],post_image_id:params[:post_image_id]).destroy
-   
+   @post_image =PostImage.find(params[:post_image_id])
+   @post_comment = @post_image.post_comments.find_by(user_id: current_user.id)
+   @post_comment.destroy
+
  end
 
  def index
