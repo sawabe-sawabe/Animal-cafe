7e06dcc572addd4336423c03e2c3ee986a349194
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-07-28 04:12:50 +0000

    修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,25 +23,25 @@
 
 $(document).ready(function () {
   $("#theTarget").skippr({
-    // スライドショーの変化 ("fade" or "slide")
+
     transition : 'fade',
-    // 変化に係る時間(ミリ秒)
-    speed : 2000,
-    // easingの種類
+
+    speed : 3000,
+
     easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
+
     navType : 'block',
-    // 子要素の種類('div' or 'img')
+
     childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
+
     arrows : false,
-    // スライドショーの自動再生(falseで自動再生なし)
+
     autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 5500,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+
+    autoPlayDuration : 5000,
+
     keyboardOnAlways : true,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+
     hidePrevious : false
   });
 });
@@ -49,25 +49,25 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $("#theTarget2").skippr({
-    // スライドショーの変化 ("fade" or "slide")
+
     transition : 'fade',
-    // 変化に係る時間(ミリ秒)
-    speed : 5000,
-    // easingの種類
+
+    speed : 2000,
+
     easing : 'easeOutQuart',
-    // ナビゲーションの形("block" or "bubble")
+
     navType : 'block',
-    // 子要素の種類('div' or 'img')
+
     childrenElementType : 'div',
-    // ナビゲーション矢印の表示(trueで表示)
-    arrows : false,
-    // スライドショーの自動再生(falseで自動再生なし)
+
+    arrows : true,
+
     autoPlay : true,
-    // 自動再生時のスライド切替間隔(ミリ秒)
+
     autoPlayDuration : 3000,
-    // キーボードの矢印キーによるスライド送りの設定(trueで有効)
+
     keyboardOnAlways : true,
-    // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
+
     hidePrevious : false
   });
 });
