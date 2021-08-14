961f4205596660ed9941f36cfdee1421d4915a37
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-08-14 02:38:47 +0000

    環境構築関係

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -8,3 +8,4 @@
 # end
 
 Refile.secret_key = 'b437169087f3bc1f108888cd7123ec0d7cc64e16459f6e5aa82cde2ea2c610ecd7c9cce3adffa651be2a1b04de0c77331de4d2c8c2c4b60a41a123894b9a7b33'
+Refile.secret_key = 'xxxxx'
